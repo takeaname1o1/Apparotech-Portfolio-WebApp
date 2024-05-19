887268c6cc9b605afab629c02d8d6cd3a2267191
takeaname1o1 <jitenkr129152@gmail.com>
2024-05-20 05:01:27 +0530

    top left logo

--- lib/view/main/components/navigation_bar.dart
+++ lib/view/main/components/navigation_bar.dart
@@ -4,6 +4,7 @@ import 'package:flutter_portfolio/view/intro/components/side_menu_button.dart';
 import 'package:flutter_portfolio/view/main/components/connect_button.dart';
 import '../../../res/constants.dart';
 import 'navigation_button_list.dart';
+
 class TopNavigationBar extends StatelessWidget {
   const TopNavigationBar({super.key});
   @override
@@ -14,13 +15,21 @@ class TopNavigationBar extends StatelessWidget {
         children: [
           const Spacer(),
           Padding(
-            padding: const EdgeInsets.all(defaultPadding),
-            child:!Responsive.isLargeMobile(context)? Image.asset('assets/images/triange_icon.png') : MenuButton(onTap: () => Scaffold.of(context).openDrawer(),),
+            padding: const EdgeInsets.all(0),
+            child: !Responsive.isLargeMobile(context)
+                ? Image.asset('assets/images/image.png')
+                : MenuButton(
+                    onTap: () => Scaffold.of(context).openDrawer(),
+                  ),
           ),
           // if(Responsive.isLargeMobile(context)) MenuButton(),
-          const Spacer(flex: 2,),
-          if(!Responsive.isLargeMobile(context))  const NavigationButtonList(),
-          const Spacer(flex: 2,),
+          const Spacer(
+            flex: 2,
+          ),
+          if (!Responsive.isLargeMobile(context)) const NavigationButtonList(),
+          const Spacer(
+            flex: 2,
+          ),
           const ConnectButton(),
           const Spacer(),
         ],
